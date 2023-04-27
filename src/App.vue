bb478f1b3f7a628561f08e38dbec9e31c7e21a5b
mirkettinho <mirkodifranco30@gmail.com>
2023-04-27 08:40:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+
+<script>
+import AppTitle from "./components/AppTitle.vue"
+export default {
+  name: "app",
+  components:{
+    AppTitle
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+
+  <AppTitle/>
+  
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+  name: "titolo",
+  data(){
+    return{
+      title: "Hello World"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1{
+  color: green;
+}
+</style>
